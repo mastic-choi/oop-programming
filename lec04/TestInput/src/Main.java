dea6dbf9de2792677d3a07adb71d7705e81ffa5e
CHOI JUNSOO <jschoi04@gmail.com>
2026-03-23 13:11:35 +0900

    update 2026.03.23.
    3월 23일 월요일 수업분

---
+++ lec04/TestInput/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+    public static void main(String[] args) {
+        Scanner myinput = new Scanner(System.in);
+        System.out.println("Enter two integers numbers : ");
+        int num1;
+        int num2;
+        num1 = myinput.nextInt();
+        num2 = myinput.nextInt();
+        System.out.println("First is "+num1+" and Second is "+num2);
+        System.out.println("The addition is "+ sum(num1, num2));
+        System.out.println("The multiplication is "+ multiply(num1, num2));
+        //end of main()
+
+    }
+
+}
